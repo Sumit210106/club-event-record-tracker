850d23427dfc28da3337e4dc4cc46a1770d0adb8
Sumit210106 <sumitnayak2006@gmail.com>
2025-02-06 12:21:24 +0530

    changed the path of keys

---
+++ config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://oskiqdthpejzihtjybwc.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9za2lxZHRocGVqemlodGp5YndjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2NTQ5NDYsImV4cCI6MjA1NDIzMDk0Nn0.1R6vsew8QNk7_vMp5P4qFNJBOfNG1Fkl9AQXS-XOSeE';
+
+
+export { SUPABASE_URL, SUPABASE_KEY };
